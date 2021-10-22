d4c7939ee797b9c51c3dd1d1fcde9a6ee1956cbf
Martha Staus <mjstaus@gmail.com>
2021-10-22 06:31:48 -0700

    initial commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const assertArraysEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
+    }
+  }
+  console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}}] === [${arr2}]`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, "3"], [1, 2, 3]);
+assertArraysEqual([1, 2], [1, 2, 3]);
+assertArraysEqual(["sandwich", 2, 3], ["sandwich", 2, 3]);
+assertArraysEqual([], []);
+assertArraysEqual([1, 2, 3, 5], [1, 2, 3, 5]);
