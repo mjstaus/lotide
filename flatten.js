74f9ac5a26a0264304d9e63d6b722cf65794935a
Martha Staus <mjstaus@gmail.com>
2021-10-22 19:45:00 -0700

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
+    return;
+  }
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
+      return;
+    }
+  }
+  console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
+};
+
+const flatten = (arr) => {
+  let flattened = []; //container variable for flattened array (to be returned)
+  for (let i = 0; i < arr.length; i ++) {
+    if (!Array.isArray(arr[i])) {
+      flattened.push(arr[i]);
+      //if not array, then push value to flattened
+    } else {
+      for (let l = 0; l < arr[i].length; l ++) {
+        flattened.push(arr[i][l]);
+        //loop through nested array values, pushing each to flattened
+      }
+    }
+  }
+  return (flattened);
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1,2,3,4,5,6]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, []]), [1,2,3,4,5]);
+assertArraysEqual(flatten([1, "5", "pony", [4, undefined]]), [1, "5", "pony", 4, undefined]);
