1babf9738af76f9bae58005357b92c6e9ac7c183
Martha Staus <mjstaus@gmail.com>
2021-10-23 15:11:42 -0700

    eslint errors fixed

--- without.js
+++ without.js
@@ -11,8 +11,8 @@ const eqArrays = (arr1, arr2) => {
 };
 
 const assertArraysEqual = (arr1, arr2) => {
-  if(eqArrays(arr1, arr2)){
-    console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`)
+  if (eqArrays(arr1, arr2)) {
+    console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {
     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
