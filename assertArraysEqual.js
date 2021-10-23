b027e0b816fbfc7c579cc53ae80ac23e9eb1c381
Martha Staus <mjstaus@gmail.com>
2021-10-22 18:59:32 -0700

    removed extra curly bracer from line 12

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,7 +9,7 @@ const assertArraysEqual = (arr1, arr2) => {
       return
     }
   }
-  console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}}] === [${arr2}]`);
+  console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
