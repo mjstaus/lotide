71094224e52ecf5d99c2a824c2de857e0780dacb
Martha Staus <mjstaus@gmail.com>
2021-10-22 17:39:36 -0700

    added returns

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,12 @@
 const assertArraysEqual = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
+    return
   }
   for (let i = 0; i < arr1.length; i ++) {
     if (arr1[i] !== arr2[i]) {
       console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
+      return
     }
   }
   console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}}] === [${arr2}]`);
