8111cad7d3e2d7a0e02f760df601bba011c93478
Martha Staus <mjstaus@gmail.com>
2021-10-25 07:02:53 -0700

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`⭐️⭐️⭐️ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔥🔥🔥 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  for (prop in object) {
+    if (object[prop] === value) {
+      return prop;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
