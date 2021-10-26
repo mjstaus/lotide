0540cb99ccbabe95a1023b6dbe66c0bf14f957b8
Martha Staus <mjstaus@gmail.com>
2021-10-26 06:31:30 -0700

    line 27: refactored equality check to single loop instead of nested loop.Line 28: Added conditional for comparing arrays as values via eqArrays function

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+// TEST/ASSERTION FUNCTIONS
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`⭐️⭐️⭐️ Assertion Passed: [${actual}] === [${expected}]`);
@@ -7,18 +7,34 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// MAIN FUNCTION
 const eqObjects = (object1, object2) => {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;  //check if the two objects have same num keys
   }
-  for (let key1 in object1) {
-    for (let key2 in object2) {
-      if (object1[key1] === object2[key2])
-        return true;
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
     }
-  }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  } return true;
 };
 
+//TEST CASES
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true);
@@ -26,5 +42,9 @@ assertEqual(eqObjects(ab, ba), true);
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false);
 
-// check if objects have same number of keys
-// loop through object 1 keys and check if object1["key"] === object2["key"]
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
