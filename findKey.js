1c0da62544864d88e8016591fea102d03bd0d28f
Martha Staus <mjstaus@gmail.com>
2021-10-30 17:00:15 -0700

    Refactored test cases to avoid magic strings/numbers (incomplete)

--- findKey.js
+++ findKey.js
@@ -13,21 +13,36 @@ const assertEqual = function(actual, expected) {
 const findKey = (object, callback) => {
   for (let key in object) {
     if (callback(object[key])) {
+      // console.log('key', callback(object[key]));
       return key;
     }
   } return undefined;
 };
 
 // TEST CASES //
-const object1 = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
+
+const BLUE_HILL = "Blue Hill";
+const AKALERI = "Akaleri";
+const NOMA = "noma";
+const ELBULLI = "elBulli";
+const ORA = "Ora";
+const AKELARRE = "Akelarre";
+
+const restaurantStars = {
+  [BLUE_HILL] : { stars: 1 },
+  [AKALERI]:   { stars: 3 },
+  [NOMA]:      { stars: 2 },
+  [ELBULLI]:   { stars: 3 },
+  [ORA]:       { stars: 2 },
+  [AKELARRE]:  { stars: 3 }
 };
 
-assertEqual(findKey(object1,  x => x.stars === 2), "noma");
-assertEqual(findKey(object1,  x => x.stars === 5), undefined);
-assertEqual(findKey(object1,  x => x.stars > 2), "Akaleri");
+assertEqual(findKey(restaurantStars,  x => x.stars === restaurantStars[NOMA].                     stars), NOMA);
+assertEqual(findKey(restaurantStars,  x => x.stars === 5), undefined);
+assertEqual(findKey(restaurantStars,  x => x.stars > 2), AKALERI);
+
+assertEqual(findKey(restaurantStars,  x => x.stars > 2), "Akaleri");
+
+//NOTES:
+// Magic string /magic number = unbound strings or numbers, not stored to variable
+//NEVER have any freely written strings or numbers floating around
