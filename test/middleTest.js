14801936adddb4150407fa33e006e6433e8cf0bb
Martha Staus <mjstaus@gmail.com>
2021-11-07 10:18:19 -0800

    refactored test code to use mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,17 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-// TEST CODE
-assertArraysEqual(middle([1, 2, 15, 4, 5, 7]), [15, 4]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle(["Ellie", "Abby", "Dina"]), ["Abby"]);
-assertArraysEqual(middle(["1", "2", "3", "4"]), ["2", "3"]);
+describe("#middle", () => {
+  it("returns [15, 4] for [1, 2, 15, 4, 5, 7]", () => {
+    assert.deepEqual(middle([1, 2, 15, 4, 5, 7]), [15, 4]);
+  });
+  it("returns emty array for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns ['Abby'] for ['Ellie', 'Abby', 'Dina]", () => {
+    assert.deepEqual(middle(["Ellie", "Abby", "Dina"]), ["Abby"]);
+  });
+  it("returns ['2', '3'] for ['1', '2', '3', '4']", () => {
+    assert.deepEqual(middle(["1", "2", "3", "4"]), ["2", "3"]);
+  });
+});
