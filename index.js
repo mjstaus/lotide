0434d6bd09870513ff43d082f8b9e3c339ab63d5
Martha Staus <mjstaus@gmail.com>
2021-11-07 14:14:59 -0800

    removed requirements except head, middle, tail

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
 const head   = require('./head');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
 const middle = require('./middle');
 const tail   = require('./tail');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  head:   head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without, 
+  head,
+  middle,
+  tail,
 };
