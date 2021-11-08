2d12527d8481b8d4122712ecfac94550d2104380
Martha Staus <mjstaus@gmail.com>
2021-11-08 06:14:30 -0800

    moved test cases to letterPositionTest file

---
+++ tests/letterPositionsTest.js
@@ -0,0 +1,10 @@
+// TEST CASES
+// const result1 = letterPositions('hello');
+// assertArraysEqual(result1['h'], [0]);
+// assertArraysEqual(result1['l'], [2, 3]);
+
+// const result2 = letterPositions('hello my name is Earl');
+// assertArraysEqual(result2["e"], [1, 12, 17]);
+// assertArraysEqual(result2["n"], [9]);
+// assertEqual(result2[" "], undefined);
+// assertEqual(result2["z"], undefined);
