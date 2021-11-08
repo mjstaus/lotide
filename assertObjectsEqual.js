38acb577792f5b67242e150bd76bb95b187db672
Martha Staus <mjstaus@gmail.com>
2021-11-07 19:10:51 -0800

    refactoring - removed eqObjects function, set to require module instead

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,28 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i ++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const eqObjects = (object1, object2) => {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;  //check if the two objects have same num keys
-  }
-  for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
-    }
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  } return true;
-};
+const eqObjects = require("./eqObjects")
 
 //TEST CODE
 const assertObjectsEqual = function(actual, expected) {
