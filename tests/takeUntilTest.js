9e8aab535c2de61cf68bc4ffcbfcb03312fd88fb
Martha Staus <mjstaus@gmail.com>
2021-11-08 07:29:32 -0800

    refactored for mocha chai

--- tests/takeUntilTest.js
+++ tests/takeUntilTest.js
@@ -1,15 +1,25 @@
-// TEST CASES //
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+const assert = require('chai').assert;
+const takeUntil = require("../takeUntil");
 
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
-
-// const data3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const result3 = takeUntil(data3, x => x > 5);
-// assertArraysEqual(result3, [1, 2, 3, 4, 5]);
-
-// const results4 = takeUntil(data3, x => x > 10);
-// assertArraysEqual(results4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+describe("#takeUntil", () => {
+  it('returns array [1, 2, 5, 7, 2] for inputs data1 and (x => x < 0)', () => {
+    const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const result1 = takeUntil(data1, x => x < 0)
+    assert.deepEqual(result1, [1, 2, 5, 7, 2]);
+  });
+  it('returns array ["I\'ve", "been", "to", "Hollywood"] for inputs data1 and (x => x < 0)', () => {
+    const data1 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    const result1 = takeUntil(data1, x => x === ',')
+    assert.deepEqual(result1, ["I've", "been", "to", "Hollywood"]);
+  });
+  it('returns array [1, 2, 3, 4, 5] for inputs data1 and (x => x < 0)', () => {
+    const data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result1 = takeUntil(data1, x => x > 5);
+    assert.deepEqual(result1, [1, 2, 3, 4, 5]);
+  });
+  it('returns array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] for inputs data1 and (x => x < 0)', () => {
+    const data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result1 = takeUntil(data1, x => x > 10);
+    assert.deepEqual(result1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+});
