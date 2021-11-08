fdaf12799953b1322ede5991904b4b005b3ef809
Martha Staus <mjstaus@gmail.com>
2021-11-08 05:49:37 -0800

    refactored for mocha chai

--- tests/findKeyTest.js
+++ tests/findKeyTest.js
@@ -1,23 +1,8 @@
+const assert = require('chai').assert;
+const findKey   = require('../findKey');
 
-// TEST CASES //
 
-// const BLUE_HILL = "Blue Hill";
-// const AKALERI = "Akaleri";
-// const NOMA = "noma";
-// const ELBULLI = "elBulli";
-// const ORA = "Ora";
-// const AKELARRE = "Akelarre";
-
-// const restaurantStars = {
-//   [BLUE_HILL] : { stars: 1 },
-//   [AKALERI]:   { stars: 3 },
-//   [NOMA]:      { stars: 2 },
-//   [ELBULLI]:   { stars: 3 },
-//   [ORA]:       { stars: 2 },
-//   [AKELARRE]:  { stars: 3 }
-// };
-
-// assertEqual(findKey(restaurantStars,  x => x.stars === restaurantStars[NOMA].                     stars), NOMA);
+//
 // assertEqual(findKey(restaurantStars,  x => x.stars === 5), undefined);
 // assertEqual(findKey(restaurantStars,  x => x.stars > 2), AKALERI);
 
@@ -26,3 +11,41 @@
 //NOTES:
 // Magic string /magic number = unbound strings or numbers, not stored to variable
 //NEVER have any freely written strings or numbers floating around
+
+describe("#findKey", () => {
+  it("returns 'noma' when passed callback for x.stars === 2", () => {
+    const restaurantStars = {
+      "Blue Hill": { stars: 1 },
+      "Akeleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    assert.strictEqual(findKey(restaurantStars,  x => x.stars === 2), "noma");
+  });
+
+  it("returns 'Akeleri' when passed callback for x.stars > 2", () => {
+    const restaurantStars = {
+      "Blue Hill": { stars: 1 },
+      "Akeleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    assert.strictEqual(findKey(restaurantStars,  x => x.stars > 2), "Akeleri");
+  });
+
+  it("returns undefined when passed callback for x.stars === 5", () => {
+    const restaurantStars = {
+      "Blue Hill": { stars: 1 },
+      "Akeleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    assert.strictEqual(findKey(restaurantStars,  x => x.stars === 5), undefined);
+  });
+});
