af484b48b2a41b38ce93bbb91daa8c542ffa95a8
Martha Staus <mjstaus@gmail.com>
2021-11-08 06:06:26 -0800

    moved test cases from flatten  to test file

--- flatten.js
+++ flatten.js
@@ -14,30 +14,4 @@ const flatten = (arr) => {
   return (flattened);
 };
 
-// TEST/ASSERTION FUNCTIONS
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i ++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
-//   }
-// };
-
-//TEST CASES
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1,2,3,4,5,6]);
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, []]), [1,2,3,4,5]);
-// assertArraysEqual(flatten([1, "5", "pony", [4, undefined]]), [1, "5", "pony", 4, undefined]);
-
 module.exports = flatten;
