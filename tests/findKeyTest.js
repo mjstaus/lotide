b10b565c9efd80069cb1c447316cb9702f1fe290
Martha Staus <mjstaus@gmail.com>
2021-11-08 05:35:21 -0800

    moved test cases from findKey

---
+++ tests/findKeyTest.js
@@ -0,0 +1,28 @@
+
+// TEST CASES //
+
+// const BLUE_HILL = "Blue Hill";
+// const AKALERI = "Akaleri";
+// const NOMA = "noma";
+// const ELBULLI = "elBulli";
+// const ORA = "Ora";
+// const AKELARRE = "Akelarre";
+
+// const restaurantStars = {
+//   [BLUE_HILL] : { stars: 1 },
+//   [AKALERI]:   { stars: 3 },
+//   [NOMA]:      { stars: 2 },
+//   [ELBULLI]:   { stars: 3 },
+//   [ORA]:       { stars: 2 },
+//   [AKELARRE]:  { stars: 3 }
+// };
+
+// assertEqual(findKey(restaurantStars,  x => x.stars === restaurantStars[NOMA].                     stars), NOMA);
+// assertEqual(findKey(restaurantStars,  x => x.stars === 5), undefined);
+// assertEqual(findKey(restaurantStars,  x => x.stars > 2), AKALERI);
+
+// assertEqual(findKey(restaurantStars,  x => x.stars > 2), "Akaleri");
+
+//NOTES:
+// Magic string /magic number = unbound strings or numbers, not stored to variable
+//NEVER have any freely written strings or numbers floating around
