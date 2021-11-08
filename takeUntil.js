6d57793a54e9f094f29915ab0797dbddb6bf809e
Martha Staus <mjstaus@gmail.com>
2021-11-08 06:41:26 -0800

    test cases moved to takeUntilTest.js

--- takeUntil.js
+++ takeUntil.js
@@ -4,7 +4,6 @@ The function will return a "slice of the array with elements taken from the begi
 To keep things simple, the callback should only be provided one value: The item in the array.
  */
 
-// MAIN FUNCTION //
 const takeUntil = (array, callback) => {
   for (let item of array) {
     if (callback(item)) {
@@ -12,42 +11,5 @@ const takeUntil = (array, callback) => {
     }
   } return array;
 };
-
-// TEST FUNCTIONS //
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i ++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
-//   }
-// };
-
-// TEST CASES //
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
-
-// const data3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const result3 = takeUntil(data3, x => x > 5);
-// assertArraysEqual(result3, [1, 2, 3, 4, 5]);
-
-// const results4 = takeUntil(data3, x => x > 10);
-// assertArraysEqual(results4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 module.exports = takeUntil;
