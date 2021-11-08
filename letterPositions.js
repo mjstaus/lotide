f12727e7d8e09a36321357cb9568245e217f5945
Martha Staus <mjstaus@gmail.com>
2021-11-08 06:26:28 -0800

    removed test cases

--- letterPositions.js
+++ letterPositions.js
@@ -1,4 +1,3 @@
-// MAIN FUNCTION
 const letterPositions = function(sentence) {
   const string = sentence.toLowerCase();
   const results = {};
@@ -11,51 +10,10 @@ const letterPositions = function(sentence) {
       results[letter].push(i);
     } else {
       results[letter] = [i];
-    }
-    
+    } 
   }
   return results;
 };
-
-// TEST/ASSERTION FUNCTIONS
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
-
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
-//   }
-// };
-
-// TEST CASES
-// const result1 = letterPositions('hello');
-// assertArraysEqual(result1['h'], [0]);
-// assertArraysEqual(result1['l'], [2, 3]);
-
-// const result2 = letterPositions('hello my name is Earl');
-// assertArraysEqual(result2["e"], [1, 12, 17]);
-// assertArraysEqual(result2["n"], [9]);
-// assertEqual(result2[" "], undefined);
-// assertEqual(result2["z"], undefined);
 
 module.exports = letterPositions;
 
