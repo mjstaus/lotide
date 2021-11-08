00dc1459f8bf4959f9ea7bca0ff7d40970231773
Martha Staus <mjstaus@gmail.com>
2021-11-08 05:53:02 -0800

    removed test cases - moving to test file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,3 @@
-
 const findKeyByValue = (object, value) => {
   for (let prop in object) {
     if (object[prop] === value) {
@@ -6,26 +5,5 @@ const findKeyByValue = (object, value) => {
     }
   }
 };
-
-//TEST/ASSERTION FUNCTIONS
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
-
-//TEST CASES
-// const bestTVShowsByGenre = {
-//   sciFi: "The Expanse",
-//   comedy: "Brooklyn Nine-Nine",
-//   drama: "The Wire",
-// };
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-// assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
 
 module.exports = findKeyByValue;
