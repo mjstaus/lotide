4c6fabab2e40a97a16491724cf6bc824dce4a824
Martha Staus <mjstaus@gmail.com>
2021-11-07 19:55:50 -0800

    refactoring countLetters test code to mocha

--- tests/countLettersTest.js
+++ tests/countLettersTest.js
@@ -1,23 +1,16 @@
 const assert = require('chai').assert;
-const head   = require('../head');
+const countLetters   = require('../countLetters');
 
-//TEST FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`⭐️⭐️⭐️ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🔥🔥🔥 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+describe("#countLetters", () => {
+  it("returns an object containing 'e': 5 for input 'NEWSFLASH: Ellie wins dog of the year award'", () => {
+    const result1 = countLetters("NEWSFLASH: Ellie wins dog of the year award")
+    assert.strictEqual(result1["e"], 5);
+  });
+});
+// const result2 = countLetters();
+// const result3 = countLetters(" ");
+// console.log(result3);
 
-
-// TEST CASES
-const result1 = countLetters("NEWSFLASH: Ellie wins dog of the year award");
-const result2 = countLetters();
-const result3 = countLetters(" ");
-console.log(result3);
-
-assertEqual(result1["e"], 5);
-assertEqual(result1["l"], 3);
-assertEqual(result2, undefined);
-assertEqual(result3, undefined);
+// assertEqual(result1["l"], 3);
+// assertEqual(result2, undefined);
+// assertEqual(result3, undefined);
