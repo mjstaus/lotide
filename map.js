93903c83488dcfb93f09d6f0fa6122ac033a7433
Martha Staus <mjstaus@gmail.com>
2021-11-08 06:27:38 -0800

    moved test cases to mapTest.js

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-//MAIN FUNCTION//
-
 /* Our map function will take in two arguments:
   - An array to map
   - A callback function
@@ -13,47 +11,6 @@ const map = function(array, callback) {
   }
   return results;
 };
-
-// TEST FUNCTIONS //
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i ++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
-//   }
-// };
-
-//TEST CASES//
-
-// const words = ["ground", "control", "to", "major", "tom"];
-// const numbers = [1, 2, 3, 4, 5];
-
-// const results1 = map(words, word => word[0]);
-// assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
-
-// const results2 = map(words, word => word.toUpperCase());
-// assertArraysEqual(results2, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
-
-// const results3 = map(numbers, num => Math.floor(num / 2));
-// assertArraysEqual(results3, [0, 1, 1, 2, 2]);
-
-// const results4 = map(numbers, num => num.toString());
-// assertArraysEqual(results4, ["1", "2", "3", "4", "5"]);
-
-// const results5 = map([], item => item[1]);
-// assertArraysEqual(results5, []);
 
 module.exports = map;
 
---
+++ tests/mapTest.js
@@ -0,0 +1,19 @@
+//TEST CASES//
+
+// const words = ["ground", "control", "to", "major", "tom"];
+// const numbers = [1, 2, 3, 4, 5];
+
+// const results1 = map(words, word => word[0]);
+// assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+
+// const results2 = map(words, word => word.toUpperCase());
+// assertArraysEqual(results2, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
+
+// const results3 = map(numbers, num => Math.floor(num / 2));
+// assertArraysEqual(results3, [0, 1, 1, 2, 2]);
+
+// const results4 = map(numbers, num => num.toString());
+// assertArraysEqual(results4, ["1", "2", "3", "4", "5"]);
+
+// const results5 = map([], item => item[1]);
+// assertArraysEqual(results5, []);
