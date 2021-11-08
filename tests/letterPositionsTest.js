11c77dcd93f4158320e7bdec13ed58156233cfea
Martha Staus <mjstaus@gmail.com>
2021-11-08 06:25:38 -0800

    refactored for mocha chai

--- tests/letterPositionsTest.js
+++ tests/letterPositionsTest.js
@@ -1,10 +1,26 @@
-// TEST CASES
-// const result1 = letterPositions('hello');
-// assertArraysEqual(result1['h'], [0]);
-// assertArraysEqual(result1['l'], [2, 3]);
+const assert = require('chai').assert;
+const letterPositions  = require('../letterPositions');
 
-// const result2 = letterPositions('hello my name is Earl');
-// assertArraysEqual(result2["e"], [1, 12, 17]);
-// assertArraysEqual(result2["n"], [9]);
-// assertEqual(result2[" "], undefined);
-// assertEqual(result2["z"], undefined);
+
+describe("#letterPositions", () => {
+  it("returns array [0] for letter 'h' of input 'hello'", () => {
+    const result1 = letterPositions("hello")
+    assert.deepEqual(result1["h"], [0]);
+  });
+  it("returns array [2,3] for letter 'l' of input 'hello'", () => {
+    const result1 = letterPositions("hello")
+    assert.deepEqual(result1["l"], [2,3]);
+  });
+  it("returns array [1, 12, 17] for letter 'e' of input 'hello my name is Earl'", () => {
+    const result1 = letterPositions("hello my name is Earl")
+    assert.deepEqual(result1["e"], [1, 12, 17]);
+  });
+  it("returns undefined for character ' ' of input 'hello my name is Earl'", () => {
+    const result1 = letterPositions("hello my name is Earl")
+    assert.deepEqual(result1[" "], undefined);
+  });
+  it("returns undefined for character 'z' of input 'hello my name is Earl'", () => {
+    const result1 = letterPositions("hello my name is Earl")
+    assert.deepEqual(result1["z"], undefined);
+  });
+});
