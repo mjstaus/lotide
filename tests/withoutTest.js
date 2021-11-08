7652ecc727b6a48a90fcd9702c7c4dc613e4dc72
Martha Staus <mjstaus@gmail.com>
2021-11-08 07:36:49 -0800

    refactored test for mocha chai

---
+++ tests/withoutTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const without = require("../without");
+
+describe("#without", () => {
+  it('returns array ["hello", "world"] for input ["hello", "world", "lighthouse"]', () => {
+    const words = ["hello", "world", "lighthouse"]
+    assert.deepEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+  });
+});
--- without.js
+++ without.js
@@ -1,4 +1,3 @@
-//MAIN FUNCTION
 const without = (source, itemsToRemove) => {
   let withoutArray = [];
   for (let i = 0; i < source.length; i++) {
@@ -15,47 +14,12 @@ const without = (source, itemsToRemove) => {
   return withoutArray;
 };
 
-// TEST/ASSERTION FUNCITONS
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i ++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`⭐️⭐️⭐️ Assertion Passed: [${arr1}] === [${arr2}]`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: [${arr1}] !== [${arr2}]`);
-//   }
-// };
-
-//TEST CODE
-// assertArraysEqual(without([1, 2, 3], [1]), [2,3]); // true
-// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); //true
-// assertArraysEqual(without([1, 2, 3], [1]), [3]); //false
-// assertArraysEqual(without([1, 2, 3], []), [1]); //false
-// assertArraysEqual(without([1], [1]), []);//true
-// assertArraysEqual(without([], []), []);//true
-
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
+//STRETCH 
 // Refactor using filter: *Filter method creates new array with all elements that pass the test implemented by the callback.
-// Callback function needs to return el !== itemsToRemove
-//
-// const without = (source, itemsToRemove) => {
+
+//// const without = (source, itemsToRemove) => {
 //   return source.filter((item) => {
 //     return !itemsToRemove.includes(item)
 //   })
-// }
 
 module.exports = without;
