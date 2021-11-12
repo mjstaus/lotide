27a5509a0136b1075cddbdf4072cdc937231ca52
Martha Staus <mjstaus@gmail.com>
2021-11-11 16:48:51 -0800

    removed function version that isn't using filter

--- tests/withoutTest.js
+++ tests/withoutTest.js
@@ -3,7 +3,7 @@ const without = require("../without");
 
 describe("#without", () => {
   it('returns array ["hello", "world"] for input ["hello", "world", "lighthouse"]', () => {
-    const words = ["hello", "world", "lighthouse"]
+    const words = ["hello", "world", "lighthouse"];
     assert.deepEqual(without(words, ["lighthouse"]), ["hello", "world"]);
   });
 });
