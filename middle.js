975986f79a9d3f79cc45ce370fc98e62a479e995
Martha Staus <mjstaus@gmail.com>
2021-11-11 16:39:41 -0800

    refactoring for clarity

--- middle.js
+++ middle.js
@@ -2,12 +2,13 @@
 // Function middle accepts an array and returns an array containing the middle-most element(s) of the array.
 
 const middle = (arr) => {
-  if (arr.length < 3) { //check if array is at least 3 (otherwise return empty array)
-    return [];
-  } else if (arr.length % 2) { //Check if arr.length divisible by 2 (will have single middle value if truthy/not div by 2)
-    return [arr[(arr.length - 1) / 2]];
+  const length = arr.length;
+  const middle = (length - 1) / 2;
+  if (length < 3) return [];
+  else if (arr.length % 2) { //Check if arr.length divisible by 2 (will have single middle value if truthy/not div by 2)
+    return [arr[middle]];
   } else {
-    return [arr[Math.floor((arr.length - 1) / 2)], arr[Math.ceil((arr.length - 1) / 2)]];
+    return [arr[Math.floor(middle)], arr[Math.ceil(middle)]];
     // For arr divisible by 2 (will have 2 middle values) - divide by 2, then use Math.floor and Math.ceil to obtain array indexes below and above the middle. Set the values of these two indexes to middleArr.
   }
 };
