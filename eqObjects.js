3d1356e20c5d10e29ecc9ae55d1276cfabae240c
Martha Staus <mjstaus@gmail.com>
2021-11-12 16:11:36 -0800

    refactored eqObjects with recursion

--- eqObjects.js
+++ eqObjects.js
@@ -1,33 +1,19 @@
 const eqArrays = require("./eqArrays");
 
-// MAIN FUNCTION
 const eqObjects = (object1, object2) => {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;  //check if the two objects have same num keys
+    return false; //check if the two objects have same num keys
   }
   for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) { //if value is an array
       return eqArrays(object1[key], object2[key]);
-    }
-    if (object1[key] !== object2[key]) {
+    } else if (typeof object1[key] === "object") { //if value is a non-array object
+      return eqObjects(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) { 
       return false;
     }
-  } return true;
+  }
+  return true;
 };
-
-//TEST CASES
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true);
-
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false);
-
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
 
 module.exports = eqObjects;
